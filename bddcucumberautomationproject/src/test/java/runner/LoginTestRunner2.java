64a307e6cd167edf7013759ff65da1018a6c98cb
Bharath <you@example.com>
2024-06-01 09:26:08 +0530

    Change the testrunner2.java

--- bddcucumberautomationproject/src/test/java/runner/LoginTestRunner2.java
+++ bddcucumberautomationproject/src/test/java/runner/LoginTestRunner2.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"features\\LoginModuleDataProvider.feature","features\\LoginModule.feature"},
+@CucumberOptions(features = {"features\\LoginModuleDataProvider.feature"},
 glue = {"step.definition"},
 plugin = {"html:htmlreport/cucumber-test-report.html","json:target/cucumber-report.json","pretty"}
 		)
